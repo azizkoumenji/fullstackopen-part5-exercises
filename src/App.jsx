4b5f539324676cff46790b3bd671199d7f1546bb
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-07-24 23:34:11 +0100

    Finished exercise 5.1

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+import Login from "./components/Login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    const user = await loginService.login({
+      username,
+      password,
+    });
+
+    window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+
+    blogService.setToken(user.token);
+    setUser(user);
+    setUsername("");
+    setPassword("");
+  };
+
+  if (user === null) {
+    return (
+      <Login username={username} handleLogin={handleLogin} password={password} setPassword={setPassword} setUsername={setUsername} />
+    )
+  } else {
+    return (
+      <div>
+        <h2>Blogs</h2>
+        <p>{user.name} is logged in.</p>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    );
+  }
+};
+
+export default App;
