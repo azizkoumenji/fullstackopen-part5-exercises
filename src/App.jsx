2f9c9d14ca65d00558f691734bcff878a4d07fc4
Mohamed Aziz Koumenji <113291493+mohamedazizkoumenji@users.noreply.github.com>
2023-07-29 20:17:46 +0100

    Finished exercise 5.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import Login from "./components/Login";
 import Add from "./components/Add";
 import Notification from "./components/Notification";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -57,8 +58,11 @@ const App = () => {
     setUser(null);
   };
 
+  const blogFormRef = useRef();
+
   const addBlog = async (event) => {
     event.preventDefault();
+    blogFormRef.current.toggleVisibility();
     const blogObject = {
       title: newTitle,
       author: newAuthor,
@@ -108,15 +112,17 @@ const App = () => {
         <Notification message={message} />
         <p>{user.name} is logged in.</p>
         <button onClick={handleLogout}>Logout</button>
-        <Add
-          handleAuthorChange={handleAuthorChange}
-          handleTitleChange={handleTitleChange}
-          handleURLChange={handleURLChange}
-          addBlog={addBlog}
-          newAuthor={newAuthor}
-          newTitle={newTitle}
-          newURL={newURL}
-        />
+        <Togglable buttonLabel="new blog" ref={blogFormRef}>
+          <Add
+            handleAuthorChange={handleAuthorChange}
+            handleTitleChange={handleTitleChange}
+            handleURLChange={handleURLChange}
+            addBlog={addBlog}
+            newAuthor={newAuthor}
+            newTitle={newTitle}
+            newURL={newURL}
+          />
+        </Togglable>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
         ))}
